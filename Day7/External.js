a2177bb1ecc031c77736778abd13dc03a4991172
HarshPatel94264 <2101031000206@silveroakuni.ac.in>
2023-06-26 15:28:05 +0530

    Day7

---
+++ Day7/External.js
@@ -0,0 +1,6 @@
+function bodyBGCP1(){
+    document.body.style.backgroundColor=document.getElementById("CP1").value;
+}
+function divBGCP2(){
+    document.getElementById("D1").style.backgroundColor=document.getElementById("CP2").value;
+}
